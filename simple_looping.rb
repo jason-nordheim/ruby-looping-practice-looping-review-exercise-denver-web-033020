561b9364f27ad7af8bf56fb9e60224d4952ad6bb
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-07 15:57:34 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -64,7 +64,7 @@ def for_iterator(number_of_times)
 
   phrase = "Welcome to Flatiron School's Web Development Course!"
 
-  for 0..number_of_times
+  for (0..number_of_times) do 
     puts phrase
   end
 end
